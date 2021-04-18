85a491192b9ad881fb2cdf7da6a324947af6e3b9
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-04-18 20:19:17 +0800

    cleanup codes 2

--- src/main/java/com/avic/mti/iron/common/config/AppConfiguration.java
+++ src/main/java/com/avic/mti/iron/common/config/AppConfiguration.java
@@ -1,8 +1,6 @@
 package com.avic.mti.iron.common.config;
 
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,8 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @since 2020-03-08, JDK1.8
  */
 @Configuration
-@RefreshScope
-@EnableEurekaClient
 @EnableCaching
 @EnableTransactionManagement
 @EnableAsync
